9a76f3b1cda1b728ed63f2dd8cbf5f64a6f97503
dani-cs <dani.cstech@gmail.com>
2021-04-04 21:17:12 +0800

    user model with user tests passed

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from datetime import date
+import random
+
+class User(AbstractUser):
+    def generate_id():
+        return str(date.today().year) + str(random.randint(100000, 999999))
+
+
+    # username becomes employeeID
+    username = models.CharField(max_length=10, primary_key=True, unique=True, default=generate_id)
+    email = models.EmailField(max_length=254, unique=True, null=False)
+    first_name = models.CharField(max_length=20, null=False)
+    last_name = models.CharField(max_length=20, null=False)
+    is_admin = models.BooleanField(default=False)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from datetime import date
+import random
+
+def generate_id():
+    return str(date.today().year) + str(random.randint(100000, 999999))
+
+class UsersManagersTests(TestCase):
+
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(username=generate_id(),
+                                        email='normal@user.com',
+                                        password='foo')
+
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.username.startswith(str(date.today().year)))
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(generate_id(),
+                                                   'super@user.com',
+                                                   'foo')
+
+        self.assertEqual(admin_user.email, 'super@user.com')
+        self.assertTrue(admin_user.username.startswith(str(date.today().year)))
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
