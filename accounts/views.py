313376868838b7748cfbb59b9a3f8564e71065dd
Rindel Subaan <valsubaan@yahoo.com>
2021-04-11 19:12:50 +0800

    added login view to views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request, 'accounts/login.html')
+
 def home(request):
     return render(request, 'accounts/dashboard.html')
