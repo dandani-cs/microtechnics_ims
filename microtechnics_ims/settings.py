f2703859f823db2de1c51c09cd75aecbb109f6f7
Roginald Dela Cruz <roginalddc22@gmail.com>
2021-04-11 19:25:51 +0800

    updates for the admin and emp side nav/base template + css, bootstrap, js + static files, icons, images

--- microtechnics_ims/settings.py
+++ microtechnics_ims/settings.py
@@ -126,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')]
