89a7c63f6c6de1a8ab20f120987fb2f9ef6900b2
Rindel Subaan <valsubaan@yahoo.com>
2021-04-11 18:39:26 +0800

    Replaced home.html to dashboard.html

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/home.html')
+    return render(request, 'accounts/dashboard.html')
