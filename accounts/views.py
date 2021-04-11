bb5cd7f2d4b4a6caa782ad413be497ed0256609d
dani-cs <dani.cstech@gmail.com>
2021-04-11 21:33:45 +0800

    created add_account view get method and template

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+class CreateUserView(View):
+    template_name = "accounts_add.html"
+
+    def get(self, request):
+        return render(request, {'form': CustomUserCreationForm()})
