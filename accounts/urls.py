c5150b7e921d8a9bf62ca78213f6b5aa269198ae
Rindel Subaan <valsubaan@yahoo.com>
2021-04-11 23:27:09 +0800

    modified accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('login/', views.login),
+    path('', views.login),
+    path('home/', views.home),
 ]
