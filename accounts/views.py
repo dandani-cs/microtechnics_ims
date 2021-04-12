284676f3e274818504bd3cf47948e9edb1e728c9
Rindel Subaan <valsubaan@yahoo.com>
2021-04-12 21:52:24 +0800

    modified login.html and forgot_pass.html template

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    return render(request, 'login.html')
 
 def forgotpass(request):
-    return render(request, 'accounts/forgot_pass.html')
+    return render(request, 'forgot_pass.html')
 
 def home(request):
     return render(request, 'dashboard.html')
