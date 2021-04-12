8acdb4747eb4b4b7a4b5d71a0064285a40f37e96
Rindel Subaan <valsubaan@yahoo.com>
2021-04-12 16:55:42 +0800

    modified accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+"""
+-- first url: login.html
+---- once logged in -> views.home
+"""
+
 urlpatterns = [
     path('', views.login),
+    path('forgot-pass/', views.forgotpass),
     path('home/', views.home),
 ]
