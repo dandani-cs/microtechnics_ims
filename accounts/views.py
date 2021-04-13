0656b9daf6fb1b71ea94ed634cb4937692bea331
dani-cs <dani.cstech@gmail.com>
2021-04-13 22:00:51 +0800

    can add user/admin

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+# for redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+
+# system info
 from .forms import CustomUserCreationForm
+from .models import User
 
 # Create your views here.
 class CreateUserView(View):
@@ -9,3 +15,20 @@ class CreateUserView(View):
 
     def get(self, request):
         return render(request, "accounts_add.html", {'form': CustomUserCreationForm()})
+
+    def post(self, request, *args, **kwargs):
+        user_form = CustomUserCreationForm(request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save(commit = False)
+
+            if user_form.cleaned_data['is_admin']:
+                user.is_staff = True
+                user.is_superuser = True
+
+            user.save()
+
+            return HttpResponseRedirect(reverse_lazy("home"))
+
+        else:
+            return render(request, "accounts_add.html", {'form': CustomUserCreationForm()})
