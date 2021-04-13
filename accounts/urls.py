41987762cf2451675ac60326155cccc9afeb79c2
Rindel Subaan <valsubaan@yahoo.com>
2021-04-13 21:43:55 +0800

    modified urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
 from . import views
 
-"""
--- first url: login.html
----- once logged in -> views.home
-
--- forgot pass? -> views.forgotpass -> enter email to get code
----- enter new pass & confirm new pass -> views.newpass
-
--- change password (user-side?)
-"""
-
 urlpatterns = [
     path('', views.login),
     path('forgot-pass/', views.forgotpass),
