5fd7af7c420f09303ebb7a5e2b838d29f1e5a4b9
dandani-cs <dani.cstech@gmail.com>
2021-04-14 13:15:28 +0800

    debugged integration of front_end and user_creation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
-    path('forgot-pass/', views.forgotpass),
-    path('recover-pass/', views.newpass),
-    path('home/', views.home),
-    path('add', CreateUserView.as_view(), name="account_add"),
+    path('forgot-pass/', views.forgotpass, name="forgot_pass"),
+    path('recover-pass/', views.newpass, name="recover_pass"),
+    path('home/', views.home, name="home"),
+    path('add', views.CreateUserView.as_view(), name="account_add"),
 ]
