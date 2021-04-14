992f9bbc34aaba06ea7d029cc43daf0fe96637ad
Uriel Federez <uafederez@gmail.com>
2021-04-14 13:49:58 +0800

    LoginRequiredMixin for ItemListView

--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from inventory.models import Item
 
 # Create your views here.
-class ItemListView(ListView):
+class ItemListView(LoginRequiredMixin, ListView):
+    login_url = 'final_login'
+    redirect_field_name = 'redirect_to'
+
     model         = Item
     template_name = "inventory/item_view.html"
