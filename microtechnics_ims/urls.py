2c7b8ea3bfb93b14be3c047d5103ecf459351e00
Roginald Dela Cruz <roginalddc22@gmail.com>
2021-04-14 13:39:15 +0800

    minor changes in admin.html + new usermanagement admin side

--- microtechnics_ims/urls.py
+++ microtechnics_ims/urls.py
@@ -19,5 +19,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="home.html"), name="home")
+    path('', TemplateView.as_view(template_name="UserMngmtAdmin.html"), name="home")
 ]
