96d72d0987e3591818717d5ead21a1150e228955
dandani-cs <dani.cstech@gmail.com>
2021-04-19 21:32:36 +0800

    purchasing_add form updated to dynamically add fields

--- purchasing/views.py
+++ purchasing/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 
-from .forms import ItemAddForm
+from .forms import ItemAddFormset
 
 
 # Create your views here.
 class CreatePurchasingView(View):
     def get(self, request):
-        return render(request, "purchasing_add.html", {'form': ItemAddForm()})
+        return render(request,
+                      "purchasing_add.html",
+                      {'formset': ItemAddFormset()})
+
+    # def post(self, request):
+    #     formset = ItemAddFormset(request.POST)
+    #     if formset.
