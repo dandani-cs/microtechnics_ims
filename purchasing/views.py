cf44368ed78a979a300e81e8c748bfaf20113fde
dandani-cs <dani.cstech@gmail.com>
2021-04-21 12:07:33 +0800

    updating unavailable options in all dropdowns

--- purchasing/views.py
+++ purchasing/views.py
@@ -4,15 +4,18 @@ from django.views import View
 from .forms import ItemAddForm, ItemFormSet
 from inventory.models import Item
 
+import json
+
 # Create your views here.
 class CreatePurchasingView(View):
     def post(self, request):
+        items = [i.item_code for i in Item.objects.all()]
         item_formset = ItemFormSet(request.POST)
 
         if item_formset.is_valid():
             print([(form.cleaned_data['item'], form.cleaned_data['quantity']) for form in item_formset])
 
-        return render(request, "purchasing_add.html", {'form': ItemFormSet()})
+        return render(request, "purchasing_add.html", {'form': ItemFormSet(), 'item_codes': json.dumps(items)})
 
     def get(self, request):
         item_formset = ItemFormSet()
