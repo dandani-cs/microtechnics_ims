d6283f81c38d09ff1b22138982cb1c5c3aafcc72
Uriel Federez <uafederez@gmail.com>
2021-04-24 10:03:15 +0800

    Added purchasing confirmation page

--- purchasing/forms.py
+++ purchasing/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from django.forms import formset_factory
+from django.db.models import Q
 
 from inventory.models import Item
+from .models import Purchasing
+
+class PurchaseConfirmForm(forms.ModelForm):
+    class Meta:
+        all_users = Item.objects.all()
+        model   = Purchasing
+        fields  = ('purchase_num', 
+                   'requested_user', 
+                   'status',
+                   'total_price' )
 
 class ItemAddForm(forms.Form):
     all_items = Item.objects.order_by('name')
 
-    item = forms.ChoiceField(choices=[(i.item_code, i.name) for i in all_items])
-    quantity = forms.DecimalField()
+    item     = forms.ChoiceField(choices=[(i.item_code, i.name) for i in all_items])
+    quantity = forms.IntegerField()
 
 ItemFormSet = formset_factory(ItemAddForm, extra = 1)
--- purchasing/views.py
+++ purchasing/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
+from decimal import *
 
-from .forms import ItemAddForm, ItemFormSet
+from .forms import ItemAddForm, ItemFormSet, PurchaseConfirmForm
+from .models import Purchasing
+
 from inventory.models import Item
 
 # Create your views here.
+class ConfirmPurchasingView(View):
+    def get(self, request):
+        #TODO: retrieve from session data
+        purchased    = { 'cc85cbafb5' : 10 , '6737aa7594' : 20 }
+        item_details = Item.objects.filter(item_code__in = purchased.keys())
+
+        for item in item_details:
+            item.quantity = purchased[item.item_code]
+
+        subtotal     = sum([ item.price * purchased[item.item_code] for item in item_details])
+        autofilled_data = {
+                'total_price'   : subtotal,
+                'items'         : purchased
+            }
+
+        purchase_form = PurchaseConfirmForm(initial = autofilled_data)
+
+        return render(request, "purchasing_confirm.html", { 
+                        'form'    : purchase_form,
+                        'item_set': item_details
+                    })
+
 class CreatePurchasingView(View):
     def post(self, request):
         item_formset = ItemFormSet(request.POST)
@@ -13,7 +39,7 @@ class CreatePurchasingView(View):
             for form in item_formset:
                 data = form.cleaned_data
                 print(data)
-        return render(request, "purchasing_add.html", {'form': ItemFormSet()})
+        return HttpResponseRedirect(reverse_lazy("purchasing_confirm"))
 
     def get(self, request):
         item_formset = ItemFormSet()
