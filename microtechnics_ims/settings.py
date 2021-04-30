0598827a630082ed6489cbc74529b87d1da04d6a
Rindel Subaan <valsubaan@yahoo.com>
2021-05-01 04:44:29 +0800

    settings.py fix

--- microtechnics_ims/settings.py
+++ microtechnics_ims/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'inventory.apps.InventoryConfig',
     'purchasing.apps.PurchasingConfig',
-    'djang_filters',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
