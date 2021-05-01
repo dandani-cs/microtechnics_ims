07c91319fd6d7c0dcce7146fd76ab4108ece40ec
dandani-cs <dani.cstech@gmail.com>
2021-05-01 18:24:52 +0800

    purchasing view added

--- purchasing/urls.py
+++ purchasing/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreatePurchasingView
+from .views import CreatePurchasingView, PurchasingListView
 
 urlpatterns = [
+    path('', PurchasingListView.as_view(), name="purchasing_view"),
     path('add', CreatePurchasingView.as_view(), name="purchasing_add")
 ]
--- purchasing/views.py
+++ purchasing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import ItemAddForm, ItemFormSet
+from .models import Purchasing
 from inventory.models import Item
 
 import json
@@ -13,7 +16,7 @@ class CreatePurchasingView(View):
         item_formset = ItemFormSet(request.POST)
 
         if item_formset.is_valid():
-            request.session['purchasing_info'] = [(form.cleaned_data['item'], form.cleaned_data['quantity']) for form in item_formset])
+            request.session['purchasing_info'] = [(form.cleaned_data['item'], form.cleaned_data['quantity']) for form in item_formset]
 
         return render(request, "purchasing_add.html", {'form': ItemFormSet(), 'item_codes': json.dumps(items)})
 
@@ -21,3 +24,11 @@ class CreatePurchasingView(View):
         items = [[i.item_code, i.name] for i in Item.objects.all()]
         item_formset = ItemFormSet()
         return render(request, "purchasing_add.html", {'form': item_formset, 'item_codes': json.dumps(items)})
+
+
+class PurchasingListView(LoginRequiredMixin, ListView):
+    login_url = 'final_login'
+    redirect_field_name = 'redirect_to'
+
+    model = Purchasing
+    template_name = "purchasing_view.html"
