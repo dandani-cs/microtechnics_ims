eb6823275e95cf6ae4cb0e1ced48134a224e2c79
Roginald Dela Cruz <roginalddc22@gmail.com>
2021-05-07 12:59:09 +0800

    Logout working + user delete prompt added

--- microtechnics_ims/settings.py
+++ microtechnics_ims/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 # Custom user model
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Password validation
