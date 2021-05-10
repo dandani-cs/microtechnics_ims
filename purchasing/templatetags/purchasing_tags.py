f6f9f9d8a000aea1351f935b177d8b826a6dbadb
dandani-cs <dani.cstech@gmail.com>
2021-05-10 16:47:53 +0800

    purchasing_details price per item * quantity added

--- purchasing/templatetags/purchasing_tags.py
+++ purchasing/templatetags/purchasing_tags.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter()
 def purchase_item_quantity(item_dict, item_code):
     return item_dict[item_code]
+
+
+@register.simple_tag()
+def calculate_price_per_item(price_per_unit, quantity):
+    return price_per_unit * quantity
